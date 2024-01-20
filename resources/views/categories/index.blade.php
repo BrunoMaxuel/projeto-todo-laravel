5f6bb565318d9da15f8254708e916b2978c30857
Bruno Maxuel <brunomaxuel2@gmail.com>
2024-01-20 15:46:37 -0300

    feita algumas alterações e feito o readme

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -9,72 +9,74 @@
             <table class="table table-responsive table-striped">
                 <thead>
                     <tr>
-                        <th style="width: 5%">
+                        <th style="width: 5%;">
                             #
                         </th>
-                        <th style="width: 20%">
+                        <th style="width: 30%;">
                             Nome
                         </th>
-                        <th style="width: 35%">
+                        <th style="width: 30%;">
                             Descrição
                         </th>
-                        <th style="width: 15%">
+                        <th style="width: 15%;">
                             Progresso
                         </th>
-                        <th style="width: 10%">
+                        <th style="width: 5%;">
                             Status
                         </th>
-                        <th style="width: 15%">
+                        <th style="width: 15%; text-align:center;">
                             Ações
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            123
-                        </td>
-                        <td>
-                            <a>
-                                Tarefas de casa
-                            </a>
-                            <br>
-                            <small>
-                                criação 01.01.2019
-                            </small>
-                        </td>
-                        <td>
-                            organizar a estrutura da tasks, fazer os cruds, organizar o controller
-                        </td>
-                        <td class="project_progress">
-                            <div class="progress progress-sm">
-                                <div class="progress-bar bg-green" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 57%">
+                    @foreach ($categories as $category)
+                        <tr>
+                            <td>
+                                {{$category->id}}
+                            </td>
+                            <td>
+                                <a>
+                                    <strong>{{$category->category_name}}</strong>
+                                </a>
+                                <br>
+                                <small>
+                                    {{$category->created_at}}
+                                </small>
+                            </td>
+                            <td>
+                                {{$category->category_description}}
+                            </td>
+                            <td class="project_progress">
+                                <div class="progress progress-sm">
+                                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 57%">
+                                    </div>
                                 </div>
-                            </div>
-                            <small>
-                                57% concluído
-                            </small>
-                        </td>
-                        <td class="project-state">
-                            <span class="badge badge-success">Ativo</span>
-                        </td>
-                        <td class="project-actions text-right">
-                            <a class="btn btn-primary btn-sm" href="#">
-                            <i class="fas fa-eye">
-
-                            </i>
-                            </a>
-                            <a class="btn btn-info btn-sm" href="#">
-                                <i class="fas fa-pen">
+                                <small>
+                                    57% concluído
+                                </small>
+                            </td>
+                            <td class="project-state">
+                                <span class="badge badge-success">Ativo</span>
+                            </td>
+                            <td class="project-actions text-right">
+                                <a class="btn btn-primary btn-sm" href="#">
+                                <i class="fas fa-eye">
 
                                 </i>
-                            </a>
-                            <a class="btn btn-danger btn-sm" href="#">
-                                <i class="fas fa-trash">
-                                </i>
-                            </a>
-                        </td>
-                    </tr>
+                                </a>
+                                <a class="btn btn-info btn-sm" href="#">
+                                    <i class="fas fa-pen">
+
+                                    </i>
+                                </a>
+                                <a class="btn btn-danger btn-sm" href="#">
+                                    <i class="fas fa-trash">
+                                    </i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,38 +41,21 @@
     </div>
     <div class="card">
         <div class="card-header ui-sortable-handle" style="cursor: move;">
-            <h3 class="card-title">
-            <i class="fas fa-chart-pie mr-1 bg-primary p-3"></i>
-                Histórico de Tarefas
-            </h3>
-            <div class="card-tools">
-                <ul class="nav nav-pills ml-auto">
-                    <li class="nav-item">
-                    <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
-                    </li>
-                </ul>
-            </div>
+            <h1 class="card-title">
+                <i class="fas fa-chart-pie mr-2 text-primary"></i>
+                Histórico do mês
+            </h1>
         </div>
         <div class="card-body">
-            <div class="tab-content p-0">
-                <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
+            <div class="tab-content">
+                <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 270px;">
                     <div class="chartjs-size-monitor">
                         <div class="chartjs-size-monitor-expand">
                             <div class="">
                             </div> 
                         </div>
-                        <div class="chartjs-size-monitor-shrink">
-                            <div class="">
-                            </div>
-                        </div>
                     </div>
-                    <canvas id="revenue-chart-canvas" height="300" style="height: 300px; display: block; width: 584px;" width="584" class="chartjs-render-monitor"></canvas>
-                </div>
-                <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
-                    <canvas id="sales-chart-canvas" height="0" style="height: 0px; display: block; width: 0px;" class="chartjs-render-monitor" width="0"></canvas>
+                    <canvas id="revenue-chart-canvas" height="270" width="700" class="chartjs-render-monitor"></canvas>
                 </div>
             </div>
         </div>
